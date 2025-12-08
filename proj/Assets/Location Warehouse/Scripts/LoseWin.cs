05c94778835f7d1a40747d5e0b56c264b44411b9
Mishel <146827253+ellyysh@users.noreply.github.com>
2025-12-09 09:55:44 +1000

    New1

---
+++ proj/Assets/Location Warehouse/Scripts/LoseWin.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject winCanvas;
+    public GameObject loseCanvas;
+
+    // КАСАНИЕ ПРЕДМЕТА ДЛЯ ПОБЕДЫ
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("WinObject"))
+        {
+            SceneManager.LoadScene("Lab");
+        }
+    }
+
+    // ЭТУ ФУНКЦИЮ БУДЕТ ВЫЗЫВАТЬ ГОБЛИН
+    public void PlayerSpotted()
+    {
+        loseCanvas.SetActive(true);
+        StartCoroutine(RestartIn5());
+    }
+
+    private System.Collections.IEnumerator RestartIn5()
+    {
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
